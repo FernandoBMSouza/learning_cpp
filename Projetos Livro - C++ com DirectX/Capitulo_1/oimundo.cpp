450a849739eee05262abbe7aa91d362a3ddc12d1
fernando_pc <fernando.bentomouradesouza@gmail.com>
2023-06-27 07:41:00 -0300

    correcao de acento no oimundo

--- Projetos Livro - C++ com DirectX/Capitulo_1/oimundo.cpp
+++ Projetos Livro - C++ com DirectX/Capitulo_1/oimundo.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+// Tudo que esta comentado serve para usar acento no gcc, alem disso, vai em utf-8 no canto inferior direito
+// e clique em reopen with encoding e selecione Western(ISO 8859-1)
+// #include <locale.h>
 
 using namespace std;
 
 int main ()
 {
+    // setlocale(LC_ALL, "Portuguese_Brazil");
     cout << "Oi mundo!\n";
     return 0;
 }
